da82326c92ee58d98c02bbc70fb61eb18cf742ae
Joselito Y. Cortes <joselitoycortes@Joselitos-iMac.local>
2026-02-09 22:34:06 +0800

    I hope this works :pray:

---
+++ ledger/views.py
@@ -0,0 +1,79 @@
+from django.shortcuts import render
+
+# Centralized data to mimic a database for now
+RECIPE_DATA = {
+    "recipes": [
+        {
+            "name": "Recipe 1",
+            "ingredients": [
+                {
+                    "name": "tomato",
+                    "quantity": "3pcs"
+                },
+                {
+                    "name": "onion",
+                    "quantity": "1pc"
+                },
+                {
+                    "name": "pork",
+                    "quantity": "1kg"
+                },
+                {
+                    "name": "water",
+                    "quantity": "1L"
+                },
+                {
+                    "name": "sinigang mix",
+                    "quantity": "1 packet"
+                }
+            ],
+            "link": "/recipe/1"
+        },
+        {
+            "name": "Recipe 2",
+            "ingredients": [
+                {
+                    "name": "garlic",
+                    "quantity": "1 head"
+                },
+                {
+                    "name": "onion",
+                    "quantity": "1pc"
+                },
+                {
+                    "name": "vinegar",
+                    "quantity": "1/2cup"
+                },
+                {
+                    "name": "water",
+                    "quanity": "1 cup"
+                },
+                {
+                    "name": "salt",
+                    "quantity": "1 tablespoon"
+                },
+                {
+                    "name": "whole black peppers",
+                    "quantity": "1 tablespoon"
+                },
+                {
+                    "name": "pork",
+                    "quantity": "1 kilo"
+                }
+            ],
+            "link": "/recipe/2"
+        }
+    ]
+}
+
+def recipe_list(request):
+    context = {"recipes": RECIPE_DATA}
+    return render(request, 'recipe_list.html', context)
+
+def recipe_1(request):
+    context = {"recipe": RECIPE_DATA["recipe1"]}
+    return render(request, 'recipe_detail.html', context)
+
+def recipe_2(request):
+    context = {"recipe": RECIPE_DATA["recipe2"]}
+    return render(request, 'recipe_detail.html', context)
