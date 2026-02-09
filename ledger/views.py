dcd13c04acbbb190d82ec1fc92f38efd4901169f
Joselito Y. Cortes <joselitoycortes@Joselitos-iMac.local>
2026-02-09 23:09:40 +0800

    :skull:

--- ledger/views.py
+++ ledger/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Centralized data to mimic a database for now
-RECIPE_DATA = {
-    "recipes": [
+
+def recipe_list(request):
+    context = { "recipes": [
         {
             "name": "Recipe 1",
             "ingredients": [
@@ -65,15 +67,70 @@ RECIPE_DATA = {
         }
     ]
 }
-
-def recipe_list(request):
-    context = {"recipes": RECIPE_DATA}
     return render(request, 'recipe_list.html', context)
 
 def recipe_1(request):
-    context = {"recipe": RECIPE_DATA["recipe1"]}
+    context = {
+    "name": "Recipe 1",
+    "ingredients": [
+        {
+            "name": "tomato",
+            "quantity": "3pcs"
+        },
+        {
+            "name": "onion",
+            "quantity": "1pc"
+        },
+        {
+            "name": "pork",
+            "quantity": "1kg"
+        },
+        {
+            "name": "water",
+            "quantity": "1L"
+        },
+        {
+            "name": "sinigang mix",
+            "quantity": "1 packet"
+        }
+    ],
+    "link": "/recipe/1"
+    }
     return render(request, 'recipe_detail.html', context)
 
 def recipe_2(request):
-    context = {"recipe": RECIPE_DATA["recipe2"]}
+    context = {
+    "name": "Recipe 2",
+    "ingredients": [
+        {
+            "name": "garlic",
+            "quantity": "1 head"
+        },
+        {
+            "name": "onion",
+            "quantity": "1pc"
+        },
+        {
+            "name": "vinegar",
+            "quantity": "1/2cup"
+        },
+        {
+            "name": "water",
+            "quantity": "1 cup"
+        },
+        {
+            "name": "salt",
+            "quantity": "1 tablespoon"
+        },
+        {
+            "name": "whole black peppers",
+            "quantity": "1 tablespoon"
+        },
+        {
+            "name": "pork",
+            "quantity": "1 kilo"
+        }
+    ],
+    "link": "/recipe/2"
+    }
     return render(request, 'recipe_detail.html', context)
