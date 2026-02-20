1144e9594acf167fa83e35a2d59c3cd12a4d02c9
Joselito Y. Cortes <joselitoycortes@Joselitos-iMac.local>
2026-02-20 23:14:25 +0800

    New feature

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient, RecipeIngredient
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 1
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientInline]
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient)
 
 # Register your models here.
